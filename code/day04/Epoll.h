7003b5d06c869ecf2039b42c19fc4f872d66b701
puck <ponepuck@gmail.com>
2023-12-13 16:54:25 +0800

    complish the packing of classes

---
+++ code/day04/Epoll.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <sys/epoll.h>
+#include <vector>
+
+class Epoll
+{
+private:
+    int epfd;
+    struct epoll_event *events;
+public:
+    //构造函数中创建epoll事件表
+    //初始化events数组
+    Epoll();
+    ~Epoll();
+    //将fd添加到epoll事件表
+    void addFd(int fd, uint32_t op);
+    //返回一个存储就绪事件的vector
+    std::vector<epoll_event> poll(int timeout = -1);
+};
+
